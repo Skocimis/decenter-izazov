e12b3561a81cc58bb8d3091b7a003370e011ccd3
VikTheTrick <viktorvujicic1@gmail.com>
2023-11-27 14:11:50 +0100

    sredjeno prosledjivanje podataka workerima, implementirana pretraga (lastRealCpdid) za velike cpdid vrednosti,dodato racunanje kamate

--- front/src/pages/CDP_Search.js
+++ front/src/pages/CDP_Search.js
@@ -5,6 +5,8 @@ import { getIlkInfo } from '@defisaver/tokens';
 
 const tokens = ["ETH-A", "WBTC-A", "USDC-A"]
 const contractAddress = '0x68C61AF097b834c68eA6EA5e46aF6c04E8945B2d'; // Adresa MakerDAO contracta
+const rateContractAddress = '0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B'
+
 const contractABI = [
     {
       "inputs": [{"internalType": "address", "name": "owner", "type": "address"}],
@@ -28,8 +30,49 @@ const contractABI = [
       "type": "function"
     }
   ];
-  
-
+const rateContractABI = [
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "name": "ilks",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "Art",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "rate",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "spot",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "line",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "dust",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+]
 function getId(rough, relative) {
   if (relative >= 2 * rough + 2) {
     return relative;
@@ -55,37 +98,34 @@ function CDP_Search() {
   const last_pushed = useRef(-1);
 
   useEffect(() => {
-    async function fetch_position(data) {
-        console.log(data.id)
-        const currentPositionData = await fetchData(data.id)
-        console.log(currentPositionData)
-        if (hexToAsciiStr(currentPositionData.ilk).trim().toLowerCase() != selectedToken.trim().toLowerCase()) {
-            console.log("lose" + hexToAsciiStr(currentPositionData.ilk).trim().toLowerCase() + "!=" + selectedToken.trim().toLowerCase())
-        const new_pushed = last_pushed.current = last_pushed.current + 1;
-        queue.current.push({ id: getId(roughCdpId, new_pushed), relative: new_pushed });
-        return;
-      }
-
-      const result =  { id: data.id, collateral: currentPositionData.collateral, amount: 1, debt: currentPositionData.debt, relative: data.relative };
-      console.log(result)
-      setPositions(e => {
-        if (e.length >= POS_N)
-          return e;
-        if (e.length == 0)
-          return [{ ...result, first: true }];
-
-        const arr = [...e, result];
-
-        if (e.length == POS_N - 1)
-          arr.sort(sortCriterium);
-
-        return arr;
-      })
-    }
     queue.current = async.queue(fetch_position, 5);
-
     return () => queue.current.kill();
   }, [])
+  async function fetch_position(data) {
+    const currentPositionData = await fetchData(data.id)
+    if (hexToAsciiStr(currentPositionData.ilk).trim() != selectedToken) {
+    const new_pushed = last_pushed.current = last_pushed.current + 1;
+    queue.current.push({ id: getId(data.cdpId, new_pushed), relative: new_pushed, cdpId: data.cdpId  });
+    return;
+  }
+
+  var debt = weiToDebtString(await fetchRateForIlk(currentPositionData.ilk)) * weiToEthString(currentPositionData.debt)
+
+  const result =  { id: data.id, collateral: weiToEthString(currentPositionData.collateral), amount: 1, debt: debt.toFixed(2), relative: data.relative };
+  setPositions(e => {
+    if (e.length >= POS_N)
+      return e;
+    if (e.length == 0)
+      return [{ ...result, first: true }];
+
+    const arr = [...e, result];
+
+    if (e.length == POS_N - 1)
+      arr.sort(sortCriterium);
+
+    return arr;
+  })
+}
   const fetchData = async (id) => {
     try {
       if (window.ethereum) {
@@ -105,7 +145,21 @@ function CDP_Search() {
       console.error('Error fetching CDP data:', error);
     }
   };
+  const fetchRateForIlk = async (ilk) => {
+    try {
+      const web3 = new Web3(window.ethereum);
+      
+      const rateContract = new web3.eth.Contract(rateContractABI, rateContractAddress);
+      
+     
+      const result = await rateContract.methods.ilks(ilk).call();
   
+      return result.rate;
+    } catch (error) {
+      console.error('Error fetching rate for ilk:', error);
+      throw error;
+    }
+  };
   function hexToAsciiStr(hexStr) {
     let asciiStr = '';
     for (let i = 0; i < hexStr.length; i += 2) {
@@ -114,10 +168,39 @@ function CDP_Search() {
         asciiStr += String.fromCharCode(parseInt(part, 16));
       }
     }
-    return asciiStr.replace(/[^\x20-\x7E]/g, '').trim(); // Uklanja sve ne-štampane karaktere i trimuje string
+    return asciiStr.replace(/[^\x20-\x7E]/g, '').trim(); 
   }
   
 
+  function weiToEthString(weiValue) {
+    const WEI_PER_ETH = BigInt(1e18);
+    const ethValue = Number(weiValue) / Number(WEI_PER_ETH);
+    return ethValue.toFixed(2);
+  }
+  function weiToDebtString(weiValue) {
+    const WEI_PER_ETH = BigInt(1e27);
+    const ethValue = Number(weiValue) / Number(WEI_PER_ETH);
+    return ethValue.toFixed(5);
+  }
+
+  async function getLastExistingCpId(cpId) {
+    let low = 1; 
+    let high = cpId; 
+  
+    while (low <= high) {
+      const mid = Math.floor((low + high) / 2);
+      const data = await fetchData(mid);
+  
+      if (data.urn !== '0x0000000000000000000000000000000000000000') {
+     
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  
+    return high > 0 ? high : 0;
+  }
   return (
     <div>
       <select
@@ -129,17 +212,36 @@ function CDP_Search() {
       <input type='text' value={roughCdpId} onChange={e => setRoughCdpId(e.target.value)}></input>
       <button onClick={async () => {
         setPositions(_ => []);
-
-        for (let i = 0; i < POS_N; i++) {
-          last_pushed.current = i;
-          queue.current.push({ id: getId(roughCdpId, i), relative: i });
+        if (queue.current) {
+            queue.current.kill(); // This clears the queue
         }
+        queue.current = async.queue(fetch_position, 5); // Re-initialize the queue
+    
+        const id = await fetchData(roughCdpId)
+
+        var lastCdpId = roughCdpId;
+            if (id.urn == '0x0000000000000000000000000000000000000000') {
+                lastCdpId = await getLastExistingCpId(roughCdpId);
+                setRoughCdpId(lastCdpId)
+            }
+
+            for (let i = 0; i < POS_N; i++) {
+                last_pushed.current = i;
+                queue.current.push({ id: getId(lastCdpId, i), relative: i, cdpId: lastCdpId });
+            }
+
       }}>Load positions</button>
         <button onClick={()=>fetchData(roughCdpId)}>Dohvati CDP Podatke</button>
 
-      {positions.map(e => <div>{e.id}</div>)}
+      {positions.map(e => 
+      <div>
+        <a>Id: {e.id}</a>
+        <a>Collateral: {e.collateral}</a>
+        <a>Debt: {e.debt}</a>
+        </div>)}
     </div>
   );
 }
 
 export default CDP_Search;
+/* global BigInt */
